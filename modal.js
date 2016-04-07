e5592cce282d86c1d2c16766fa8585c7adddf439
Adrian Unger <dev@staydecent.ca>
2016-04-07 13:29:00 -0700

    Positively test show and close method.

--- modal.js
+++ modal.js
@@ -36,7 +36,7 @@
 
   function modalService($document, $compile, $controller, $http, $rootScope, $q, $timeout, $templateCache) {
 
-    //  get the body of the document, we'll add the modal to it
+    // get the body of the document, we'll add the modal to it
     var body = $document.find('body');
     var $ = angular.element;
 
@@ -44,10 +44,10 @@
 
     function ModalInstance() {
       this.show = function(options) {
-        //  create a deferred we'll resolve when the modal is ready.
+        // create a deferred we'll resolve when the modal is ready.
         var deferred = $q.defer();
 
-        //  validate the input parameters.
+        // validate the input parameters.
         if (!options) {
           deferred.reject("No options have been specified.");
           return deferred.promise;
@@ -59,52 +59,50 @@
           return deferred.promise;
         }
 
-        //  get the actual html of the template.
+        // get the actual html of the template.
         getTemplate(options.template, options.templateUrl)
           .then(function(template) {
 
-            //  create a new scope for the modal.
+            // create a new scope for the modal.
             var modalScope = $rootScope.$new();
 
-            //  Create the inputs object to the controller - this will include
-            //  the scope, as well as all inputs provided.
-            //  We will also create a deferred that is resolved with a provided
-            //  close function. The controller can then call 'close(result)'.
-            //  The controller can also provide a delay for closing - this is
-            //  helpful if there are closing animations which must finish first.
+            // Create the inputs object to the controller - this will include the scope, 
+            // as well as all inputs provided. We will also create a deferred that is 
+            // resolved with a provided close function. The controller can then call 
+            // 'close(result)'. The controller can also provide a delay for closing - 
+            // this is helpful if there are closing animations which must finish first.
             var closeDeferred = $q.defer();
             var inputs = {
               $scope: modalScope,
               close: function(result, delay) {
-                if (!delay) {
-                  delay = 0;
-                }
+                delay = delay || 0;
                 $timeout(function () {
                   closeDeferred.resolve(result);
                 }, delay);
+                return closeDeferred.promise;
               }
             };
 
-            //  If we have provided any inputs, pass them to the controller.
-            if(options.inputs) {
-              for(var inputName in options.inputs) {
+            // If we have provided any inputs, pass them to the controller.
+            if (options.inputs) {
+              for (var inputName in options.inputs) {
                 inputs[inputName] = options.inputs[inputName];
               }
             }
 
-            //  Parse the modal HTML into a DOM element (in template form).
+            // Parse the modal HTML into a DOM element (in template form).
             var modalElementTemplate = angular.element(template);
 
-            //  Compile then link the template element, building the actual element.
-            //  Set the $element on the inputs so that it can be injected if required.
+            // Compile then link the template element, building the actual element.
+            // Set the $element on the inputs so that it can be injected if required.
             var linkFn = $compile(modalElementTemplate);
             var modalElement = linkFn(modalScope);
             inputs.$element = modalElement;
 
-            //  Create the controller, explicitly specifying the scope to use.
+            // Create the controller, explicitly specifying the scope to use.
             var modalController = $controller(controller, inputs);
 
-            //  Finally, append the modal to the dom.
+            // Finally, append the modal to the dom.
             body.append(modalElement);
 
             // Add 'on' class after timeout so transition happens
@@ -112,7 +110,7 @@
               $(modalElement).addClass('on');
             }, 100);
 
-            //  We now have a modal object.
+            // We now have a modal object.
             var modal = {
               controller: modalController,
               scope: modalScope,
@@ -139,17 +137,20 @@
             });
             */
 
-            $(modalElement).find('.app-modal-overlay').click(function(event) {
-              event.preventDefault();
-              closeDeferred.resolve();
-            });
+            var $overlay = $(modalElement).find('.app-modal-overlay');
+            if ($overlay.length) {
+              $overlay.click(function(event) {
+                event.preventDefault();
+                closeDeferred.resolve();
+              });
+            }
 
-            //  When close is resolved, we'll clean up the scope and element.
+            // When close is resolved, we'll clean up the scope and element.
             modal.close.then(function() {
-              //  Clean up the scope
+              // Clean up the scope
               modalScope.$destroy();
 
-              //  Remove the element from the dom.
+              // Remove the element from the dom.
               $(modalElement).removeClass('on');
               $timeout(function(){
                 modalElement.remove();
@@ -166,34 +167,38 @@
         return deferred.promise;
       };
 
-      //  returns a promise which gets the template, either
-      //  from the template parameter or via a request to the
-      //  template url parameter.
+      // returns a promise which gets the template, either from the template parameter 
+      // or via a request to the template url parameter.
       function getTemplate(template, templateUrl) {
         var deferred = $q.defer();
-        if(template) {
+
+        if (template) {
           deferred.resolve(template);
         } else if(templateUrl) {
-          // check to see if the template has already been loaded
+          // Load the cached template or request it for the first time
           var cachedTemplate = $templateCache.get(templateUrl);
-          if(cachedTemplate !== undefined) {
+          
+          if (cachedTemplate !== undefined) {
             deferred.resolve(cachedTemplate);
-          }
-          // if not, let's grab the template for the first time
-          else {
-            $http({method: 'GET', url: templateUrl, cache: true})
-              .then(function(result) {
-                // save template into the cache and return the template
-                $templateCache.put(templateUrl, result.data);
-                deferred.resolve(result.data);
-              })
-              .catch(function(error) {
-                deferred.reject(error);
-              });
+          } else {
+            $http({
+              method: 'GET', 
+              url: templateUrl, 
+              cache: true
+            })
+            .then(function(result) {
+              // save template into the cache and return the template
+              $templateCache.put(templateUrl, result.data);
+              deferred.resolve(result.data);
+            })
+            .catch(function(error) {
+              deferred.reject(error);
+            });
           }
         } else {
           deferred.reject("No template or templateUrl has been specified.");
         }
+
         return deferred.promise;
       }
     }
--- test/modal.spec.js
+++ test/modal.spec.js
@@ -4,12 +4,16 @@ var modal = require('../modal.js');
 describe('factory: modalService', function() {
   var $rootScope;
   var $scope;
+  var $q;
+  var $timeout;
   var modalService;
 
   beforeEach(angular.mock.module(modal));
 
-  beforeEach(inject(function(_$rootScope_, _modalService_) {
+  beforeEach(inject(function(_$rootScope_, _$q_, _$timeout_, _modalService_) {
     $rootScope = _$rootScope_;
+    $q = _$q_;
+    $timeout = _$timeout_;
     modalService = _modalService_;
     $scope = $rootScope.$new();
   }));
@@ -48,5 +52,43 @@ describe('factory: modalService', function() {
 
       $scope.$apply();
     });
+
+    it('should show with a template string', function(done) {
+      modalService.show({
+        controller: function() {},
+        template: '<h1 id="myModal">OKOKOKOK</h1>'
+      })
+      .then(function(modal) {
+        assert.isOk(document.getElementById('myModal'));
+        done();
+      })
+      .catch(done);
+
+      $scope.$apply();
+    });
+
+    it('should close modal after showing', function(done) {
+      var modalResult = 'result';
+
+      modalService.show({
+        controller: function($scope, close) {
+          close(modalResult, 0).then(function(result) {
+            assert.equal(result, modalResult);
+          });
+        },
+        template: '<h1 id="myModal">OKOKOKOK</h1>'
+      })
+      .then(function(modal) {
+        $q.when(modal.close, function(result) {
+          assert.equal(result, modalResult);
+          done();
+        });
+      })
+      .catch(done);
+
+      $scope.$apply();
+      $timeout.flush();
+    });
+
   });
 });
